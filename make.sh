9edf11d1cdf80efc63095904ea3ecc00aa2400a0
lpm0073 <lpm0073@gmail.com>
2022-05-17 09:59:32 -0500

    Cookiecutter scaffolding

---
+++ make.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+#------------------------------------------------------------------------------
+# written by:   mcdaniel
+#               https://lawrencemcdaniel.com
+#
+# date:         mar-2022
+#
+# usage:        Re-runs the Cookiecutter for this repository.
+#------------------------------------------------------------------------------
+
+GITHUB_REPO="gh:lpm0073/cookiecutter-openedx-devops"
+GITHUB_BRANCH="main"
+OUTPUT_FOLDER="../"
+
+cookiecutter --checkout $GITHUB_BRANCH \
+             --output-dir $OUTPUT_FOLDER \
+             --overwrite-if-exists \
+             --no-input \
+             $GITHUB_REPO \
+             global_platform_name=mrionline \
+             global_platform_region=usa \
+             global_aws_region=us-east-2 \
+             global_account_id=621672204142 \
+             global_root_domain=mrionline.com \
+             global_aws_route53_hosted_zone_id=Z0476367ACNR8F3YFZX \
+             environment_name=live \
+             environment_subdomain=dev
---
+++ terraform/environments/global.hcl
@@ -0,0 +1,33 @@
+#------------------------------------------------------------------------------
+# written by: Lawrence McDaniel
+#             https://lawrencemcdaniel.com/
+#
+# date: Feb-2022
+#
+# usage: create global parameters, exposed to all
+#        Terragrunt modules in this repository.
+#------------------------------------------------------------------------------
+locals {
+  platform_name    = "mrionline"
+  platform_region  = "usa"
+  root_domain      = "mrionline.com"
+  aws_region       = "us-east-2"
+  account_id       = "621672204142"
+  ec2_ssh_key_name = "mrionline-live"
+
+  tags = {
+    Platform        = local.platform_name
+    Platform-Region = local.platform_region
+    Terraform       = "true"
+  }
+
+}
+
+inputs = {
+  platform_name    = local.platform_name
+  platform_region  = local.platform_region
+  aws_region       = local.aws_region
+  account_id       = local.account_id
+  root_domain      = local.root_domain
+  ec2_ssh_key_name = local.ec2_ssh_key_name
+}
